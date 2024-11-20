a0267402ee9718ba186a2cf6b070c7679838cef6
Bi5cuitOliva <marlonjojo83@gmail.com>
2024-11-20 21:26:40 +0300

    the usermiddleware The middleware checks if the authenticated user's usertype is 'admin'.

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserMiddleware
@@ -15,6 +16,11 @@ class UserMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if(Auth::user()->usertype == 'admin'){
+            return $next($request);
+
+        }
+
+        return redirect()->back();
     }
 }
