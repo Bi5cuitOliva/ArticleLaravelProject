b7629dcccdb5ddcb05fc0b6c5dec8f09f5f67c0b
Bi5cuitOliva <marlonjojo83@gmail.com>
2024-11-20 21:36:13 +0300

    created a user controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,4 +22,6 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'userMiddleware'])->group(function(){
 
+
+    Route::get('dashboard', [UserController::class, 'index'])->name('dashboard');
 });
